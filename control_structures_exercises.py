e09779db69ff8a06f39e2ae9bc0841760407e2f5
Ray Cerna <ray.cerna01@gmail.com>
2022-04-08 16:03:59 -0500

    updated exercises

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -25,7 +25,7 @@ else:
 #You get paid time and a half if you work more than 40 hours
 
 num_hours=65
-hour_wage=20
+hour_wage=100
 
 if num_hours<=40:
     paycheck=num_hours*hour_wage
@@ -33,3 +33,53 @@ if num_hours<=40:
 else:
     paycheck=(num_hours-40)*1.5*hour_wage+40*hour_wage
     print("Weekley pay is: "+str(paycheck))
+
+#2 Loop Basics
+
+#a. While
+    # Create an integer variable i with a value of 5.
+    # Create a while loop that runs so long as i is less than or equal to 15
+    # Each loop iteration, output the current value of i, then increment i by one.
+
+i=5
+while i<=15:
+    print(i)
+    i+=1  
+
+# Create a while loop that will count by 2's starting with 0 and ending at 100. 
+# Follow each number with a new line.
+
+for i in range(0,102,2):
+    print(i)
+
+i=0    
+while i<=100:
+    print(i)
+    i+=2
+
+# Alter your loop to count backwards by 5's from 100 to -10.
+
+for i in range(100,-10,-5):
+    print(i)
+
+i=100
+while i>=-10:
+    print(i)
+    i-=5
+
+# Create a while loop that starts at 2, and displays the number squared on each line
+#  while the number is less than 1,000,000. Output should equal:
+# 2
+# 4
+# 16
+# 256
+# 65536
+
+i=2
+while i<1000000:
+    print(i)
+    i=i**2
+    
+
+
+
