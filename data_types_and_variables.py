cd19705e769d98a28de5d10fc8d17239f16c1fdb
Ray Cerna <ray.cerna01@gmail.com>
2022-04-08 12:13:33 -0500

    adding data_types_and_variablesexercises

---
+++ data_types_and_variables.py
@@ -0,0 +1,15 @@
+# You have rented some movies for your kids: 
+# The little mermaid (for 3 days), 
+# Brother Bear (for 5 days, they love it), and 
+# Hercules (1 day, you don't know yet if they're going to like it). 
+# If price for a movie per day is 3 dollars, how much will you have to pay?
+
+# When do you know you need a variable?
+# Variables are the nouns in our programs
+
+price_per_day = 3
+little_mermaid_days = 3
+brother_bear_days = 5
+hercules_days = 1
+total_days = little_mermaid_days + brother_bear_days + hercules_days
+total_days
