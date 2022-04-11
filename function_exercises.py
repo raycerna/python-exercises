9ae07053ef784a31c947c83365079244c6870710
Ray Cerna <ray.cerna01@gmail.com>
2022-04-11 15:59:42 -0500

    adding function exercises

---
+++ function_exercises.py
@@ -0,0 +1,9 @@
+# 1 Define a function named is_two. 
+# It should accept one input and return True if the 
+# passed input is either the number or the string 2, False otherwise.
+
+def istwo(num):
+    if int(num)==2:
+        return True
+    else:
+        return False 
