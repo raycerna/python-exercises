3f89c3f8713bf2692b01060930feeb9423c550bc
Ray Cerna <ray.cerna01@gmail.com>
2022-04-12 17:01:45 -0500

    added looping exercises

---
+++ looping_problems.py
@@ -0,0 +1,186 @@
+students = [
+    {
+        "id": "100001",
+        "student": "Ada Lovelace",
+        "coffee_preference": "light",
+        "course": "web development",
+        "grades": [70, 91, 82, 71],
+        "pets": [{"species": "horse", "age": 8}],
+    },
+    {
+        "id": "100002",
+        "student": "Thomas Bayes",
+        "coffee_preference": "medium",
+        "course": "data science",
+        "grades": [75, 73, 86, 100],
+        "pets": [],
+    },
+    {
+        "id": "100003",
+        "student": "Marie Curie",
+        "coffee_preference": "light",
+        "course": "web development",
+        "grades": [70, 89, 69, 65],
+        "pets": [{"species": "cat", "age": 0}],
+    },
+    {
+        "id": "100004",
+        "student": "Grace Hopper",
+        "coffee_preference": "dark",
+        "course": "data science",
+        "grades": [73, 66, 83, 92],
+        "pets": [{"species": "dog", "age": 4}, {"species": "cat", "age": 4}],
+    },
+    {
+        "id": "100005",
+        "student": "Alan Turing",
+        "coffee_preference": "dark",
+        "course": "web development",
+        "grades": [78, 98, 85, 65],
+        "pets": [
+            {"species": "horse", "age": 6},
+            {"species": "horse", "age": 7},
+            {"species": "dog", "age": 5},
+        ],
+    },
+    {
+        "id": "100006",
+        "student": "Rosalind Franklin",
+        "coffee_preference": "dark",
+        "course": "data science",
+        "grades": [76, 70, 96, 81],
+        "pets": [],
+    },
+    {
+        "id": "100007",
+        "student": "Elizabeth Blackwell",
+        "coffee_preference": "dark",
+        "course": "web development",
+        "grades": [69, 94, 89, 86],
+        "pets": [{"species": "cat", "age": 10}],
+    },
+    {
+        "id": "100008",
+        "student": "Rene Descartes",
+        "coffee_preference": "medium",
+        "course": "data science",
+        "grades": [87, 79, 90, 99],
+        "pets": [{"species": "cat", "age": 10}, {"species": "cat", "age": 8}],
+    },
+    {
+        "id": "100009",
+        "student": "Ahmed Zewail",
+        "coffee_preference": "medium",
+        "course": "data science",
+        "grades": [74, 99, 93, 89],
+        "pets": [{"species": "cat", "age": 0}, {"species": "cat", "age": 0}],
+    },
+    {
+        "id": "100010",
+        "student": "Chien-Shiung Wu",
+        "coffee_preference": "medium",
+        "course": "web development",
+        "grades": [82, 92, 91, 65],
+        "pets": [{"species": "cat", "age": 8}],
+    },
+    {
+        "id": "100011",
+        "student": "William Sanford Nye",
+        "coffee_preference": "dark",
+        "course": "data science",
+        "grades": [70, 92, 65, 99],
+        "pets": [{"species": "cat", "age": 8}, {"species": "cat", "age": 5}],
+    },
+    {
+        "id": "100012",
+        "student": "Carl Sagan",
+        "coffee_preference": "medium",
+        "course": "data science",
+        "grades": [100, 86, 91, 87],
+        "pets": [{"species": "cat", "age": 10}],
+    },
+    {
+        "id": "100013",
+        "student": "Jane Goodall",
+        "coffee_preference": "light",
+        "course": "web development",
+        "grades": [80, 70, 68, 98],
+        "pets": [{"species": "horse", "age": 4}],
+    },
+    {
+        "id": "100014",
+        "student": "Richard Feynman",
+        "coffee_preference": "medium",
+        "course": "web development",
+        "grades": [73, 99, 86, 98],
+        "pets": [{"species": "dog", "age": 6}],
+    },
+]
+
+#how many pets does each student have?
+
+for student in students:
+    number_of_pets = len(student["pets"])
+    print(f"{student['student']} has {number_of_pets} pet(s)")
+
+len(students)
+
+#How many students prefer light coffee? Fore each type of cofee roast?
+
+light_coffee_drinkers = [0]
+
+for student in students:
+    if student["coffee_preference"] == "light":
+        light_coffee_drinkers +=1
+
+light_coffee_drinkers
+
+
+coffee_preferences = {}
+
+student = students[0]
+
+preference = student["coffee_preference"]
+preference
+
+if preference not in coffee_preferences.key():
+    coffee_preferences[preference] +=1
+else:
+    preference
+
+preference
+
+roasts = {}
+
+for student in students:
+    preference = student["coffee_preference"]
+    if preference in roasts.key():
+        roasts[preference] += 1
+    else:
+        roasts[preference] = 1
+roasts
+
+
+#how many types of each pet are there?
+
+pets = []
+
+student = students[1]
+for pet in student["pets"]:
+    pets.append(pet["species"])
+
+pets
+
+pets = []
+for student in students:
+    for pet in student["pets"]:
+        pets.append(pet["species"])
+pets
+
+pet_species_counts = {}
+
+for pet in pets:
+    if pet in pet_species_counts.keys():
+        pet_species_counts[pet] += 1
+    else:
+        pet_species_counts
