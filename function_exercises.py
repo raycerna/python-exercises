96fa6e1d9667ff8785de7c251d85a95b21c1e7ef
Ray Cerna <ray.cerna01@gmail.com>
2022-04-12 10:18:46 -0500

    updated exercises

--- function_exercises.py
+++ function_exercises.py
@@ -1,9 +1,85 @@
-# 1 Define a function named is_two. 
-# It should accept one input and return True if the 
-# passed input is either the number or the string 2, False otherwise.
+# 1 
 
-def istwo(num):
-    if int(num)==2:
+from ast import Num
+
+
+def is_two(x):
+    if int(x)==2:
         return True
     else:
         return False 
+
+# 2 
+def is_vowel(x):
+  if x in ["a","e","i","o","u","A","E","I","O","U"]:
+    return True
+  else:
+    return False
+
+def is_vowel(x):
+    if x in ['a','e','i','o','u']:
+        return True
+    else:
+        return False
+
+# 3
+
+def is_consonant(x):
+     if is_vowel(x)==True:
+         return False
+     else:
+         return True 
+
+# 4
+
+def captialize_consonant(word):
+    if is_consonant(word[0])==True:
+        return word[0].upper()+word[1:]
+    else:
+         return word
+
+# 5
+
+def calculate_tip(tip_per,bill):
+    return (tip_per/100*bill)
+
+calculate_tip(10,159)
+
+# 6
+
+def apply_discount(dis_per,orig_price):
+    discount = orig_price * (dis_per/100)
+    return orig_price - discount
+
+apply_discount(20, 199)
+
+# 7
+
+def handle_commas(number):
+    return int(number.replace(',', ''))
+
+handle_commas("999,999,999")
+
+def handle_commas(some_input):
+    if type(some_input) == str:
+        return some_input.replace(",", "")
+    return some_input
+
+some_input(9999999)
+
+
+#8
+
+def get_letter_grade(num):
+    num = int(num)
+    if num >= 90:
+        return('A')
+    elif num >= 80:
+        return('B')
+    elif num >= 70:
+        return('C')
+    elif num >= 60:
+        return('D')
+    else:
+        return('F')
+        
