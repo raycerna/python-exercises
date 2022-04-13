95f2ff0b167cc7caf58435100c956e47237ccf90
Ray Cerna <ray.cerna01@gmail.com>
2022-04-13 16:51:06 -0500

    updaded import ex .py

--- import_exercises.py
+++ import_exercises.py
@@ -1,3 +1,13 @@
+#1
 import function_exercises as fe
 from function_exercises import calculate_tip
 calculate_tip(27,0.1)
+
+#2
+import itertools
+itertools.product("abc", "123")
+list(itertools.product('abc', '123'))
+
+list(itertools.combinations("abcd", 2))
+
+list(itertools.permutations("abcd", 2))
